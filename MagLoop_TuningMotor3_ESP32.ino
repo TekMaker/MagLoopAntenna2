8bab3e36cfa6b214fbce9786db147817031aa46f
TekMakerUK <76254383+TekMaker@users.noreply.github.com>
2023-05-03 19:58:37 +0100

    Added numerical indicator for reqPos

--- MagLoop_TuningMotor3_ESP32.ino
+++ MagLoop_TuningMotor3_ESP32.ino
@@ -8,11 +8,11 @@
 #include <RemoteXY.h>
 
 // RemoteXY connection settings
-#define REMOTEXY_WIFI_SSID "your wifi name"
-#define REMOTEXY_WIFI_PASSWORD "your password"
+#define REMOTEXY_WIFI_SSID "your SSID" /// put your credentials here
+#define REMOTEXY_WIFI_PASSWORD "your password"/// put your credentials here
 #define REMOTEXY_CLOUD_SERVER "cloud.remotexy.com"
 #define REMOTEXY_CLOUD_PORT 6376
-#define REMOTEXY_CLOUD_TOKEN "your remoteXY cloud token"
+#define REMOTEXY_CLOUD_TOKEN "your API Key"/// put your credentials here
 
 // wifi credentials
 const char* ssid = REMOTEXY_WIFI_SSID;
@@ -20,25 +20,23 @@ const char* password = REMOTEXY_WIFI_PASSWORD;
 
 // remoteXY vars
 uint8_t stepSize = 1;
-int8_t lastSlider = 0;
-int8_t sliderValue = 0;
 int16_t reqPos = 0;  // required position
 int16_t limit = 270;
 int16_t curPos = 0;  // current capacitor position
 
 // RemoteXY configurate
 #pragma pack(push, 1)
-uint8_t RemoteXY_CONF[] =  // 183 bytes
-  { 255, 4, 0, 3, 0, 176, 0, 16, 31, 1, 72, 20, 11, 19, 43, 43, 1, 6, 140, 38,
+uint8_t RemoteXY_CONF[] =  // 192 bytes
+  { 255, 4, 0, 14, 0, 185, 0, 16, 31, 1, 72, 20, 11, 19, 43, 43, 1, 6, 140, 38,
     0, 0, 0, 0, 0, 128, 132, 67, 0, 0, 0, 0, 1, 0, 3, 63, 12, 12, 1, 31,
     68, 101, 99, 114, 101, 97, 115, 101, 0, 129, 0, 3, 3, 56, 6, 6, 77, 97, 103, 76,
     111, 111, 112, 32, 67, 111, 110, 116, 114, 111, 108, 108, 101, 114, 0, 129, 0, 10, 91, 46,
     6, 6, 70, 105, 110, 101, 32, 65, 100, 106, 117, 115, 116, 109, 101, 110, 116, 0, 1, 0,
-    48, 63, 12, 12, 12, 31, 73, 110, 99, 114, 101, 97, 115, 101, 0, 129, 0, 21, 55, 23,
+    48, 63, 12, 12, 12, 31, 73, 110, 99, 114, 101, 97, 115, 101, 0, 129, 0, 21, 66, 23,
     6, 6, 80, 111, 115, 105, 116, 105, 111, 110, 0, 129, 0, 8, 10, 44, 6, 6, 98, 121,
     32, 84, 101, 107, 109, 97, 107, 101, 114, 85, 75, 0, 1, 0, 14, 76, 12, 12, 2, 31,
     45, 45, 0, 1, 0, 35, 76, 12, 12, 4, 31, 43, 43, 0, 70, 16, 28, 36, 9, 9,
-    26, 37, 0 };
+    26, 37, 0, 67, 4, 23, 53, 20, 5, 1, 16, 11 };
 
 // this structure defines all the variables and events of your control interface
 struct {
@@ -50,6 +48,8 @@ struct {
   // output variables
   int16_t circularbar_1;  // from 0 to limit
   uint8_t led_1;          // led state 0
+  char text_1[11];        // string UTF8 end zero
+
 
   // other variable
   uint8_t connect_flag;  // =1 if wire connected, else =0
@@ -63,10 +63,10 @@ struct {
 //////////////////////////////////////////////////////////////////////////////
 //  Declare variables for the motor pins
 //////////////////////////////////////////////////////////////////////////////
-uint8_t motorPin1 = 26;  // Blue - 28BYJ48 pin 1 (Arduino)
-uint8_t motorPin2 = 25;  // Pink - 28BYJ48 pin 2
-uint8_t motorPin3 = 33;  // Yellow - 28BYJ48 pin 3
-uint8_t motorPin4 = 32;  // Orange - 28BYJ48 pin 4
+uint8_t motorPin1 = 27;  // Blue - 28BYJ48 pin 1 (Arduino)
+uint8_t motorPin2 = 26;  // Pink - 28BYJ48 pin 2
+uint8_t motorPin3 = 25;  // Yellow - 28BYJ48 pin 3
+uint8_t motorPin4 = 33;  // Orange - 28BYJ48 pin 4
 //                                                                                      // Red - 28BYJ48 pin 5 (VCC)
 int motorSpeed = 3600;                                                               // 800 - 4800 defaultvariable to set stepper speed, ***higher is slower***
 int lookup[8] = { B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001 };  // used for stepping the motor
@@ -94,23 +94,23 @@ void RemoteXYHandler() {
 
   // right button pressed
   if (RemoteXY.button_right == 1) {
-    reqPos = (reqPos + sliderValue) + stepSize * 5;
+    reqPos = reqPos + (stepSize * 5);
     RemoteXY.led_1 = 1;
   }
   // left button pressed
   if (RemoteXY.button_left == 1) {
-    reqPos = (reqPos + sliderValue) - stepSize * 5;
+    reqPos = reqPos - (stepSize * 5);
     RemoteXY.led_1 = 1;
   }
 
   // right button2 pressed, fine control
   if (RemoteXY.button_right2 == 1) {
-    reqPos = (reqPos + sliderValue) + stepSize;
+    reqPos = reqPos + stepSize;
     RemoteXY.led_1 = 1;
   }
   // left button2 pressed, fine control
   if (RemoteXY.button_left2 == 1) {
-    reqPos = (reqPos + sliderValue) - stepSize;
+    reqPos = reqPos - stepSize;
     RemoteXY.led_1 = 1;
   }
 
@@ -124,10 +124,15 @@ void RemoteXYHandler() {
   }
   Serial.println(reqPos);
   RemoteXY.circularbar_1 = reqPos;
+  String str = String(reqPos);
+  uint8_t len = str.length() + 1;
+  char char_array[len];
+  str.toCharArray(char_array, 11); // display on reqPos app
+  //strcpy  (RemoteXY.text_1, str);
+  sprintf(RemoteXY.text_1, char_array);
+  
   Serial.println();
   delay(250);
-
-  lastSlider = sliderValue;
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -138,7 +143,7 @@ void getEncoderPosition() {
   bool decSW = digitalRead(pinSW);
 
   if (!decSW) {
-    if (stepSize == 200) {
+    if (stepSize == 8) {
       stepSize = 1;
       Serial.println("Stepsize 1");
       delay(500);
@@ -304,11 +309,8 @@ void loop() {
 
   bool remote = false;
 
-  // if (remote) {
   RemoteXYHandler();  // use WiFi AP
-  // } else {
-  // getEncoderPosition();  // if using digital encoder
-  // }
-  // move the motor
+  //getEncoderPosition();  // if using digital encoder
+
   moveMotor();
 }
